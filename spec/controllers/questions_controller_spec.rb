569af129ac6678c8432819af06f76594c93d4a62
fsiscar <fsiscar@gmail.com>
2018-04-05 13:03:26 -0500

    Assignment 19: best efford - checkbox not working

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe QuestionsController, type: :controller do
 
     it "assigns my_question to @question" do
       get :show, params: { id: my_question.id }
-      expect(assigns(:post)).to eq(my_question)
+      expect(assigns(:question)).to eq(my_question)
     end
   end
 
@@ -55,12 +55,12 @@ RSpec.describe QuestionsController, type: :controller do
 
     it "assigns the new question to @question" do
        question :create, params: { question: { title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: [true, false].sample } }
-       expect(assigns(:post)).to eq Post.last
+       expect(assigns(:question)).to eq Question.last
      end
 
      it "redirects to the new question" do
        question :create, params: { question: { title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved:[true, false].sample } }
-       expect(response).to redirect_to Post.last
+       expect(response).to redirect_to Question.last
      end
   end
 
@@ -85,32 +85,44 @@ RSpec.describe QuestionsController, type: :controller do
       expect(question_instance.body).to eq my_question.body
       expect(question_instance.resolved).to eq my_question.resolved
     end
+  end
 
 
-    describe "PUT update" do
-      it "updates question with expected attributes" do
-        new_title = RandomData.random_sentence
-        new_body = RandomData.random_paragraph
-        new_resolved = [true, false].sample
+  describe "PUT update" do
+    it "updates question with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+      new_resolved = [true, false].sample
 
-        put :update, params: { id: my_question.id, question: {title: new_title, body: new_body, resolved: new_resolved } }
+      put :update, params: { id: my_question.id, question: {title: new_title, body: new_body, resolved: new_resolved } }
 
-        updated_question = assigns(:question)
-        expect(updated_question.id).to eq my_question.id
-        expect(updated_question.title).to eq new_title
-        expect(updated_question.body).to eq new_body
-        expect(updated_question.resolved).to eq new_resolved
-      end
+      updated_question = assigns(:question)
+      expect(updated_question.id).to eq my_question.id
+      expect(updated_question.title).to eq new_title
+      expect(updated_question.body).to eq new_body
+      expect(updated_question.resolved).to eq new_resolved
+    end
 
-      it "redirects to the updated question" do
-        new_title = RandomData.random_sentence
-        new_body = RandomData.random_paragraph
-        new_resolved = [true, false].sample
+    it "redirects to the updated question" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+      new_resolved = [true, false].sample
 
-        put :update, params: { id: my_question.id, question: {title: new_title, body: new_body, resolved: new_resolved } }
-        expect(response).to redirect_to my_question
-      end
+      put :update, params: { id: my_question.id, question: {title: new_title, body: new_body, resolved: new_resolved } }
+      expect(response).to redirect_to my_question
     end
   end
 
+  describe "DELETE destroy" do
+    it "deletes the question" do
+      delete :destroy, params: { id: my_question.id }
+      count = Question.where({id: my_question.id}).size
+      expect(count).to eq 0
+    end
+
+    it "redirects to question index" do
+      delete :destroy, params: { id: my_question.id }
+      expect(response).to redirect_to question_path
+    end
+  end
 end
