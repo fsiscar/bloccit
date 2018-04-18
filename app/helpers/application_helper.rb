b9e748be705aaa42df4f7a585d0b192f733b6e3b
fsiscar <fsiscar@gmail.com>
2018-04-17 22:01:47 -0500

    Checkpoint 22:Rails Calidations: completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def form_group_tag(errors, &block)
+    ccs_class = 'form-group'
+    ccs_class << ' has-error' if errors.any?
+    content_tag :div, capture(&block), class: ccs_class
+  end
 end
