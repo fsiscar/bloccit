5791f3df2a06ef746a032c449dd92214134fdcd5
fsiscar <fsiscar@gmail.com>
2018-04-23 16:15:24 -0500

    Assignment 24:this is before watching the solution video

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,11 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
 
   get 'contact' => 'welcome#contact'
-
-  get 'faq' => 'welcome#faq'
 
   root 'welcome#index'
 
