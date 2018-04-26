2e3cf31484348ee65f4d0098ea04893902bde453
fsiscar <fsiscar@gmail.com>
2018-04-26 13:12:10 -0500

    Assignment 26:Rails:Posts and Users:completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,11 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
+  scope :ordered_by_title, -> { order('title DESC')}
+  scope :ordered_by_reverse_created_at, ->{ order('created_at ASC')}
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
-
-  scope :ordered_by_title
-  scope :ordered_by_reverse_created_at
 end
