7ed067d93a4c2e0d0207bd06eea7aad93333d646
fsiscar <fsiscar@gmail.com>
2018-05-01 21:08:42 -0500

    Checkpoint 29:Rails:Voting:Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :questions
-  resources :advertisements
   resources :topics do
     resources :posts, except: [:index]
   end
 
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
+    post '/up_vote' => 'votes#up_vote', as: :up_vote
+    post '/down_vote' => 'votes#down_vote', as: :down_vote
   end
 
   resources :users, only: [:new, :create]
