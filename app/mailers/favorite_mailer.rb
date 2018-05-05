bc1e0f15ce7852d5ab8dcf8b0b3468d92b6adb45
fsiscar <fsiscar@gmail.com>
2018-05-05 15:36:09 -0500

    Assignment 30:Rails:Favorites:Completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,14 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, subject: "New comment on #{post.title}")
    end
+
+   def new_post(post)
+     headers["Message-ID"] = "<posts/#{post.id}@shrouded-woodland-49260.herokuapp.com>"
+     headers["In-Reply-To"] = "<post/#{post.id}@shrouded-woodland-49260.herokuapp.com>"
+     headers["References"] = "<post/#{post.id}@shrouded-woodland-49260.herokuapp.com>"
+
+     @post = post
+
+     mail(to: post.user.email, subject: "You are following your newly created post: #{post.title}")
+   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,8 @@ class Post < ApplicationRecord
 
   default_scope { order('rank DESC') }
 
+  after_create :favorite_own_post
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
@@ -30,5 +32,12 @@ class Post < ApplicationRecord
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def favorite_own_post
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 end
